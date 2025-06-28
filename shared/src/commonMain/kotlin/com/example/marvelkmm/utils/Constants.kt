f59f0931005090d31c62ab45bc2a7d0f0bcb472c
nlizarraga9 <nicolaslizarraga152@gmail.com>
2025-06-28 17:46:51 -0300

    error apikeys

--- shared/src/commonMain/kotlin/com/example/marvelkmm/utils/Constants.kt
+++ shared/src/commonMain/kotlin/com/example/marvelkmm/utils/Constants.kt
@@ -1,4 +1,4 @@
 package com.example.marvelkmm.utils
 
-internal val PUBLIC_KEY = "f55c1e6aa878479bbd6a99f950138050"
-internal val PRIVATE_KEY = "361a3161178ff96f387bad534392343449c9e204"
+internal val PUBLIC_KEY = "6583b4d7b1639dc4f8f15fc4c9b6268e"
+internal val PRIVATE_KEY = "c5ca0184639c9d0b2194f0487dbcc577231b2792"
